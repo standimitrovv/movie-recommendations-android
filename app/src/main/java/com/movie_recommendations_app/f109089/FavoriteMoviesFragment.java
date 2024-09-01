d563c2d37a09888b177c4d099c394bc8f206e2bc
Stanimir Dimitrov <evlution63@gmail.com>
2024-09-01 15:37:29 +0300

    feat: create a bottom navigation menu

---
+++ app/src/main/java/com/movie_recommendations_app/f109089/FavoriteMoviesFragment.java
@@ -0,0 +1,19 @@
+package com.movie_recommendations_app.f109089;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FavoriteMoviesFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.favorite_movies_fragment, container, false);
+    }
+}
--- app/src/main/java/com/movie_recommendations_app/f109089/MainActivity.java
+++ app/src/main/java/com/movie_recommendations_app/f109089/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +23,29 @@ public class MainActivity extends AppCompatActivity {
             FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             fragmentTransaction.add(R.id.main, movieListFragment);
             fragmentTransaction.commit();
-
         }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int id = item.getItemId();
+
+            if (id == R.id.navigation_all_movies) {
+                selectedFragment = new MovieListFragment();
+            } else if (id == R.id.navigation_favorites) {
+                selectedFragment = new FavoriteMoviesFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
 
         Intent serviceIntent = new Intent(this, MovieUpdateService.class);
         startService(serviceIntent);
